6d4816d7f9d25c8f39f3d3060858325b35470c4d
teleclient <54826170+teleclient@users.noreply.github.com>
2020-09-13 08:29:58 -0700

    Update credentials.php

--- public/credentials.php
+++ public/credentials.php
@@ -1,7 +1,7 @@
 <?php
 return [
     // webwarp
-    'api_id'       => 904912,
+    'api_id'       => 904945,
     'api_hash'     => '8208f08eefc502bedea0987437123456',
     'acc_password' => '',
     'acc_phone'    => 19498765456,
