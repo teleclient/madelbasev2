75f89f21c0fea627f09b2425bf5454df6bb5bbd4
teleclient <54826170+teleclient@users.noreply.github.com>
2020-09-18 10:30:17 -0700

    Remove real credentials.

---
+++ public/credentials.sample.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    // webwarp
+    'api_id'       => 904945,
+    'api_hash'     => '8208f08eefc502bedea0987437123456',
+    'acc_password' => '',
+    'acc_phone'    => 19498765456,
+    'bot_token'    => '1039597234:AAEQ1zd1k-WhweDNtcNck098765o-123456',
+
+];
